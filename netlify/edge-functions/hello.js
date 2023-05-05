cf8586e2c8fcc5cf136aa8a636ebb2ccd205f021
John Li <no-reply@github.com>
2023-05-05 13:35:01 +1000

    test 1

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,5 +1,14 @@
-export default async (request) => {
-  return new Response("Hello, World!", {
-    headers: { "content-type": "text/html" },
+export default async (request, context) => {
+  const clientId = Deno.env.get("CLIENT_ID");
+  const clientSecret = Deno.env.get("CLIENT_SECRET");
+  const resp = await fetch("https://integrationapi.borderexpress.com.au/token", {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Accept": "application/json"
+    },
+    method: 'POST',
+    body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`
   });
+  const jsonData = await resp.json();
+  return Response.json(jsonData);
 };
