3c96ab814fbc1eda04fb9e160269100c41e6e4fc
John Li <no-reply@github.com>
2023-05-08 17:38:59 +1000

    x-api-key

--- netlify/edge-functions/bexToken.js
+++ netlify/edge-functions/bexToken.js
@@ -1,14 +1,19 @@
 export default async (request, context) => {
-  const clientId = Deno.env.get("CLIENT_ID");
-  const clientSecret = Deno.env.get("CLIENT_SECRET");
-  const resp = await fetch("https://integrationapi.borderexpress.com.au/token", {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      "Accept": "application/json"
-    },
-    method: 'POST',
-    body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`
-  });
-  const jsonData = await resp.json();
-  return Response.json(jsonData);
+  const xKey = request.headers.get('X-API-Key')
+  if (xKey === 'secretKey234') {
+    const clientId = Deno.env.get("CLIENT_ID");
+    const clientSecret = Deno.env.get("CLIENT_SECRET");
+    const resp = await fetch("https://integrationapi.borderexpress.com.au/token", {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept": "application/json"
+      },
+      method: 'POST',
+      body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`
+    });
+    const jsonData = await resp.json();
+    return Response.json(jsonData);
+  } else {
+    return Response.json({ message: 'invalid request' });
+  }
 };
